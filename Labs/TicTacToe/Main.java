b95b3f52ca5375e491a69b4e0461167a3e263ec3
Izzy Lopez <71158214+Izzyxiii@users.noreply.github.com>
2020-09-12 18:02:59 -0700

    Add files via upload

---
+++ Labs/TicTacToe/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // write your code here
+        System.out.println("TicTacToe\n");
+
+        TicTacToe t = new TicTacToe();
+        t.printBoard();
+        t.play();
+
+        t.printBoard();
+        t.play();
+
+        t.printBoard();
+
+    }
+}
+
+
--- Labs/TicTacToe/TicTacToe.java
+++ Labs/TicTacToe/TicTacToe.java
@@ -1,52 +1,93 @@
 import java.util.Scanner;
 
-public class TicTacToe 
+public class TicTacToe
 {
-	private final int BOARDSIZE = 3; 
-	private enum Status {WIN, DRAW, CONTINUE}; 
-	private char [][]board; 
-	private boolean firstPlayer; 
-	private boolean gameOver; 
-	private int turn = 0; 
-	
-	TicTacToe()
-	{
-		this.board = new char[BOARDSIZE][BOARDSIZE]; 
-		firstPlayer = false; 
-		gameOver = false; 
-	}
-	
-	public void play()
-	{	
-		Scanner keyboard = new Scanner(System.in); 
-		
-		firstPlayer = true; 
-		
-		System.out.println("Enter row: 0, 1, 2"); 
-		
-		int row = keyboard.nextInt(); 
-		System.out.println(); 
-		
-		System.out.println("Enter col: 0, 1, 2"); 
-		int col = keyboard.nextInt(); 
-		
-		boolean move = validMove(row, col); 
-		printBoard(); 
-	}
+    // constants
+    private final int BOARDSIZE = 3;
 
-	public void printBoard()
-	{
-		for(int i = 0; i < BOARDSIZE; i++) {
-			System.out.print("| ");
-			for(int j = 0; j < BOARDSIZE; j++) {
-				System.out.print(" |" + " ");
-			}
-			System.out.println(); 
-		}
-	}
-	
-	private boolean validMove(int row, int column)
-	{
-		return row >= 0 && row < BOARDSIZE && column >= 0 && column < BOARDSIZE; 
-	}
+    // instance variables
+    private enum Status {WIN, DRAW, CONTINUE};
+    private char [][]board;
+    private boolean firstPlayer;
+    private boolean gameOver;
+    private int playerTurn = 1;
+
+    public TicTacToe()
+    {
+        board = new char[BOARDSIZE][BOARDSIZE];
+        firstPlayer = true;
+        gameOver = false;
+    }
+
+    public void play()
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        // state which player's turn it is
+        if (firstPlayer)
+            System.out.println("Player X's turn.");
+        else
+            System.out.println("Player O's turn.");
+
+        // get the user's input for row & col
+        System.out.print("Enter row: 0, 1, 2: ");
+        int row = keyboard.nextInt();
+
+        System.out.print("Enter col: 0, 1, 2: ");
+        int col = keyboard.nextInt();
+
+        System.out.println();
+
+
+        // make sure their move is valid
+        // otherwise re-prompt the user for input
+        if (!validMove(row, col))
+        {
+            System.out.print("This is an invalid move, try again.\n");
+            play();
+        }
+        else
+            {
+                // firstPlayer = X
+                // secondPlayer = O
+                if (firstPlayer)
+                {
+                    board[row][col] = 'X';
+                    firstPlayer = false;
+                }
+                else {
+                    board[row][col] = 'O';
+                    firstPlayer = true;
+                }
+            }
+    }
+
+    public void printBoard()
+    {
+        for(int i = 0; i < BOARDSIZE; i++) {
+            System.out.print("| ");
+            for(int j = 0; j < BOARDSIZE; j++) {
+                System.out.print(printSymbol(i, j));
+                System.out.print(" |" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private char printSymbol(int row, int col)
+    {
+        return board[row][col];
+    }
+
+    private boolean validMove(int row, int col)
+    {
+        // if the row/col is not empty return false
+        // otherwise return true
+        if (board[row][col] != '\0')
+        {
+            return false;
+        }
+        return true;
+        //return row >= 0 && row < BOARDSIZE && column >= 0 && column < BOARDSIZE;
+    }
 }
