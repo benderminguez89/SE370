3e5ffd7a3fa4bfcfd97855d9625790a08db6bc62
Izzy <71158214+Izzyxiii@users.noreply.github.com>
2020-10-02 22:40:17 -0700

    Adding the source files for the Random Bar Chart Program

---
+++ Labs/Random Bar Chart/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    /**
+     * Main method for the random bar chart
+     *
+     * @author Joel Plotnik, Rebecca Norwood, Ismael Lopez (Izzy), Benjamin Dominguez
+     * @since 09-20-2020
+     * @param args String
+     */
+    public static void main(String[] args)
+    {
+        // call Graph class to implement Swing
+        Graph g = new Graph();
+    }
+}
